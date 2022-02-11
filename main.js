10d26fb303e81882a4f4ce6718d4870c99a99df4
Ezavala17 <98781087+Ezavala17@users.noreply.github.com>
2022-02-10 19:04:51 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+
+
+
+function init() 
+{
+    var canvas = document.getElementById("webgl-canvas");
+    gl = canvas.getContext("webgl2");
+    
+    gl.clearColor(0.0, 0.5, 0.0, 1.0);
+
+    cone = new Cone(gl, 20, "Cone-vertex-shader", "Cone-fragment-shader");
+    render();
+}
+    function render() 
+    {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        cone.render();
+                
+    };
+    
+    window.onload = init;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
